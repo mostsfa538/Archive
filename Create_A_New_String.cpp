7ddc17d0a96a4d7715cab7ae91bd9db7756a19ee
mostsfa538 <111648823+mostsfa538@users.noreply.github.com>
2023-07-27 23:45:10 +0300

    Create_A_New_String.cpp

---
+++ Create_A_New_String.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#define Mokhtar ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long
+using namespace std;
+int main() {
+    Mokhtar;
+    string st1;
+    string st2;
+    getline(cin,st1);
+    getline(cin,st2);
+    int z=st1.size(),x=st2.size();
+    cout<<z<<" "<<x<<endl<<st1<<" "<<st2;
+}
